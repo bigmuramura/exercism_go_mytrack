718f93ea934d5434e0115036a8d6dce46df4a7d6
bigmuramura <nishikimachi.hokkaido@gmail.com>
2020-04-11 22:19:16 +0900

    無駄な空欄チェック削除、forループをシンプルな記述に変更

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -10,16 +10,13 @@ func Distance(a, b string) (int, error) {
 	// Slice the string as a rune type
 	ar, br := []rune(a), []rune(b)
 
-	if a == "" || b == "" {
-		if a != b {
-			return 0, errors.New("one is blank")
-		}
-	} else if len(ar) != len(br) {
+	// Checking the number of slices
+	if len(ar) != len(br) {
 		return 0, errors.New("the number of characters is different")
 	}
 
 	ans := 0
-	for i := 0; i < len(ar); i++ {
+	for i := range ar {
 		if ar[i] != br[i] {
 			ans++
 		}
